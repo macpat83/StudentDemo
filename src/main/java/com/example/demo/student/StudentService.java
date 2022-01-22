21340136dffb4a89470e979b8b456b8c73614bce
macpat83 <macpat@gmail.com>
2022-01-22 16:54:21 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Miriam",
+                        "miriam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+
+                )
+
+        );
+    }
+
+}
